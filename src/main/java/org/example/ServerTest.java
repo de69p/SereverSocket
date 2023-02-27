10666ec910e27dc2c9b4753c42c344269b72de59
Arsen <de69p@icloud.com>
2023-02-27 13:37:17 +0200

    Initial commit

--- src/main/java/org/example/ServerTest.java
+++ src/main/java/org/example/ServerTest.java
@@ -23,28 +23,22 @@ public class ServerTest {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
 
-        // Отримуємо привітання від сервера
         String response = reader.readLine();
         Assertions.assertEquals("Привіт, напиши щось українською мовою:", response);
 
-        // Надсилаємо повідомлення з російськими буквами
         writer.write("Привет!\n");
         writer.flush();
 
-        // Отримуємо запит на визначення "паляниці"
         response = reader.readLine();
         Assertions.assertEquals("Що таке паляниця? Напиши відповідь:", response);
 
-        // Надсилаємо правильну відповідь
         writer.write("Це хліб.\n");
         writer.flush();
 
-        // Отримуємо поточну дату та час
         response = reader.readLine();
         String expectedResponse = "Правильно! Ось поточна дата та час: ";
         Assertions.assertTrue(response.startsWith(expectedResponse));
 
-        // Закриваємо з'єднання з сервером
         serverSocket.close();
     }
 
@@ -59,20 +53,16 @@ public class ServerTest {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
 
-        // Отримуємо привітання від сервера
         String response = reader.readLine();
         Assertions.assertEquals("Привіт, напиши щось українською мовою:", response);
 
-        // Надсилаємо повідомлення з українськими буквами
         writer.write("Добрий день!\n");
         writer.flush();
 
-        // Отримуємо поточну дату та час
         response = reader.readLine();
         String expectedResponse = "Дякую за привітання. Ось поточна дата та час: ";
         Assertions.assertTrue(response.startsWith(expectedResponse));
 
-        // Закриваємо з'єднання з сервером
         serverSocket.close();
     }
 
@@ -86,18 +76,15 @@ public class ServerTest {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
-        // надсилаємо клієнту рядок для відповіді
+
         writer.write("Що таке паляниця? Напиши відповідь:\n");
         writer.flush();
 
-        // надсилаємо неправильну відповідь
         writer.write("Це пиріг.");
         writer.flush();
 
-        // читаємо відповідь від сервера
         String response = reader.readLine();
 
-        // перевіряємо, що сервер повідомив про неправильну відповідь та відключив клієнта
         Assertions.assertTrue(response.contains("Неправильна відповідь."));
         Assertions.assertEquals(-1, clientSocket.getInputStream().read());
     }
